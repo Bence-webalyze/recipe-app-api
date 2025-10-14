2d6bcb94c832c700b7a8b6b2506a79c9acb5c690
Fazek007 <rigobence616@gmail.com>
2025-10-14 12:23:50 +0200

    add postgresql to docker

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        res = calc.add(5, 4)
+
+        self.assertEqual(res, 9)
+
+    def test_subract_numbers(self):
+        """Substracting numbers"""
+
+        res = calc.substract(10, 15)
+
+        self.assertEqual(res, 5)
